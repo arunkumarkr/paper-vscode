418bee0839adecc374059f717ff784ca8696bd89
Arun <beetroot@192.168.1.11>
2025-04-20 08:37:49 +0530

    Add Notes

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { NotesTreeDataProvider } from "./notesTreeData";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "paper-vscode" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "paper-vscode" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "paper-vscode.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from paper-vscode!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('paper-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from paper-vscode!');
-	});
+  const notesProvider = new NotesTreeDataProvider(context);
 
-	context.subscriptions.push(disposable);
+  vscode.window.createTreeView("notesView", {
+    treeDataProvider: notesProvider,
+  });
+
+  const openNoteCommand = vscode.commands.registerCommand(
+    "paper.openNote",
+    (uri: vscode.Uri) => {
+      vscode.workspace.openTextDocument(uri).then((doc) => {
+        vscode.window.showTextDocument(doc);
+      });
+    }
+  );
+
+  const newNoteCommand = vscode.commands.registerCommand(
+    "paper.newNote",
+    () => {
+      notesProvider.createNewNote();
+    }
+  );
+
+  context.subscriptions.push(disposable, openNoteCommand, newNoteCommand);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/notesTreeData.ts
@@ -0,0 +1,66 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+
+export class NotesTreeDataProvider
+  implements vscode.TreeDataProvider<NoteItem>
+{
+  private notesDir: string;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    NoteItem | undefined | void
+  > = new vscode.EventEmitter<NoteItem | undefined | void>();
+  readonly onDidChangeTreeData: vscode.Event<NoteItem | undefined | void> =
+    this._onDidChangeTreeData.event;
+
+  constructor(private context: vscode.ExtensionContext) {
+    this.notesDir = path.join(context.globalStorageUri.fsPath, "notes");
+    if (!fs.existsSync(this.notesDir)) {
+      fs.mkdirSync(this.notesDir, { recursive: true });
+    }
+  }
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: NoteItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(): Thenable<NoteItem[]> {
+    const files = fs
+      .readdirSync(this.notesDir)
+      .filter((file) => file.endsWith(".txt"));
+    const items = files.map((file) => {
+      const filePath = path.join(this.notesDir, file);
+      return new NoteItem(file, vscode.Uri.file(filePath));
+    });
+    return Promise.resolve(items);
+  }
+
+  createNewNote() {
+    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    const fileName = `note-${timestamp}.txt`;
+    const filePath = path.join(this.notesDir, fileName);
+    fs.writeFileSync(filePath, "", "utf8");
+    this.refresh();
+    vscode.workspace.openTextDocument(filePath).then((doc) => {
+      vscode.window.showTextDocument(doc);
+    });
+  }
+}
+
+class NoteItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly resourceUri: vscode.Uri
+  ) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.command = {
+      command: "paper.openNote",
+      title: "Open Note",
+      arguments: [this.resourceUri],
+    };
+    this.contextValue = "noteItem";
+  }
+}
